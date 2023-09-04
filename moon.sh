ac91c88dccf48be3396557c30d99019d9272af9e
Garten <brilliangarten@gmail.com>
2023-09-04 15:12:25 +0700

    Update moon.sh

--- moon.sh
+++ moon.sh
@@ -8,7 +8,7 @@ export LC_ALL=C && export USE_CCACHE=1
 ccache -M 120G
 export ARCH=arm64
 export KBUILD_BUILD_HOST=GearCI
-export KBUILD_BUILD_USER="DPSLEGEND"
+export KBUILD_BUILD_USER="Gartenn"
 git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-6443078 clang
 git clone --depth=1 https://gitlab.com/firefly-linux/prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.git los-4.9-64
 git clone --depth=1 https://github.com/MayuriLabs/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
@@ -27,7 +27,7 @@ make -j$(nproc --all) O=out \
 
 function zupload()
 {
-git clone --depth=1 https://github.com/DPSLEGEND/Anykernel3.git -b moon AnyKernel
+git clone --depth=1 https://github.com/Gartenn/Anykernel3.git -b moon AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
 zip -r9 Test-DolphinKernel-moon.zip *
